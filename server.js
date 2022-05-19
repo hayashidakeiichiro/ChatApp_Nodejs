005e97aa06c8a8506bff75ed13c74b4589e41eb6
hayashidakeiichiro <keiichi444666@gmail.com>
2022-05-19 14:47:30 +0900

    my-first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+io.on("connection", (socket) => {
+    console.log("Usser connected");
+
+    socket.on("chat message", (msg)=>{
+        console.log(msg);
+        io.emit("chat message", msg);
+    });
+});
+server.listen(8080, () => {
+    console.log("Success!")
+});
